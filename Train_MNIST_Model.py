9fb7204027b43ebd6bfd0632ba8096053116bb18
Chris Cui <cuicaihao@users.noreply.github.com>
2020-02-04 12:16:41 +1100

    :zap:update and check python codes, add earlystop function in callback list

--- Train_MNIST_Model.py
+++ Train_MNIST_Model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on Friday 06-09-2019
+Created on Friday 2 Feb 2020
 
 @author: Chris.Cui
 
@@ -16,7 +16,7 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 import os
 import numpy as np
@@ -25,19 +25,13 @@ from datetime import datetime
 import pickle
 
 #%% Load the data
-
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 nb_train_sample = len(y_train)
 nb_validation_sample = len(y_test)
 
-#%% 
-
-batch_size = 1024
-num_classes = 10
-epochs = 10
-
+#%% Preporcessing the data
 # input image dimensions
 img_rows, img_cols = 28, 28
 if K.image_data_format() == 'channels_first':
@@ -58,20 +52,21 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
+num_classes = 10
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
 #%% design the structure of the model
 model = Sequential()
-model.add(Conv2D(16, kernel_size=(3, 3),
+model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
 model.add(Conv2D(16, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
@@ -82,14 +77,25 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
 print(model.summary())
 
-#%%
+#%% Train the model
 # checkpoint
-now = datetime.now()
-best_model_filepath="mnist.weights.best."+str( now.strftime("%Y.%m.%d_%H.%M.%S"))+".h5"
-checkpoint = ModelCheckpoint(best_model_filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
+# now = datetime.now()
+# best_model_filepath="mnist.weights.best."+str( now.strftime("%Y.%m.%d_%H.%M.%S"))+".h5"
+
+best_model_filepath = "./models/mnist.weights.best.h5"
+checkpoint = ModelCheckpoint(best_model_filepath, 
+                            monitor='val_acc', 
+                            verbose=1, save_best_only=True, mode='Max')
+
+stoppoint = EarlyStopping(monitor='val_acc', min_delta=0.001,
+                            verbose=1, mode='Max', patience=5)
+
+callbacks_list = [checkpoint, stoppoint]
 
 # Fit the model
+batch_size = 1024
+epochs = 30
+
 print('*'*80)
 print('Start the training process at:', datetime.now())
 print('*'*80)
@@ -104,9 +110,16 @@ print('*'*80)
 print('End the training process at:', datetime.now())
 print('*'*80)
 # save the training records
-pickle_out = open("history.pickle","wb")
+pickle_out = open("./models/history.pickle","wb")
 pickle.dump(history, pickle_out)
 
+# *** output on my desktop
+# ...
+# Epoch 00020: val_acc did not improve from 0.99210
+# Epoch 00020: early stopping
+# ***
+# End the training process at: 2020-02-04 10:33:54.029526
+# ***
 #%%
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
@@ -122,7 +135,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 # plt.show()
-plt.savefig('training_validation_accuracy_values.png', dpi= 300 )
+plt.savefig('./images/training_validation_accuracy_values.png', dpi= 300 )
 
 # Plot training & validation loss values
 plt.figure(figsize=(8, 6))
@@ -133,26 +146,17 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 # plt.show()
-plt.savefig('training_validation_loss_values.png', dpi= 300 )
+plt.savefig('./images/training_validation_loss_values.png', dpi= 300 )
 
 
-#%%
-now=datetime.now()
-save_model_dir = './models/'
-final_model_name = str( now.strftime("%Y.%m.%d_%H.%M.%S")) +'.MnistModel.h5'
-save_final_model_path = os.path.join(save_model_dir, final_model_name)
-# model.save('first_try.h5')
-model.save(save_final_model_path)
+#%% save and reload the model with a timestamp / in case overight the original one
+# now=datetime.now()
+# save_model_dir = './models/'
+# final_model_name = str(now.strftime("%Y.%m.%d_%H.%M.%S")) +'.MnistModel.h5'
+# save_final_model_path = os.path.join(save_model_dir, final_model_name)
+# model.save(save_final_model_path)
 
-
-#%% 
-
-Final_model =   load_model(save_final_model_path) # maybe overfitting.
-Final_model.summary()
+# Final_model =   load_model(save_final_model_path) # maybe overfitting.
+# Final_model.summary()
  
-#%%
-
-Best_model =   load_model(best_model_filepath)
-Best_model.summary()
-
-#%%
+#%% add more ...
